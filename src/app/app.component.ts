7b20dad875c3b6af91df5d5958c6a9656ed3cfe8
chatkeon <chatkeon@gmail.com>
2023-05-30 10:33:18 -0700

    Fixed bug with footnote indicators; added instructions; clean-up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,45 +6,59 @@ import { Component, ViewChild } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private readonly HEADING_REGEX = new RegExp('(.+)(\n[0-9]+ )', 'g');
-  private readonly FOOTNOTE_REGEX = new RegExp('\[[a-z0-9]+]', 'gi');
-  private readonly FIRST_NUMBER_REGEX = new RegExp('([0-9]+) ');
-  private readonly VERSE_NUMBER_REGEX = new RegExp('^[0-9]+');
+  private readonly HEADING_REGEX: RegExp = new RegExp('\n([^\s\n]+)(\n[0-9]+ )', 'g');
+  private readonly FIRST_NUMBER_REGEX: RegExp = new RegExp('([0-9]+) ');
+  private readonly VERSE_NUMBER_REGEX: RegExp = new RegExp('^[0-9]+');
+  private readonly FOOTNOTE_REGEX: RegExp = new RegExp('(\\[[a-z]+\\])+', 'gi');
 
   removeVerseNumbers: boolean = false;
   removeHeadings: boolean = false;
-  removeFootnotes: boolean = false;
+  removeFootnotes: boolean = true;
   insertNewlines: boolean = true;
+  preserveWhitespace: boolean = true;
   results: string = '';
+  currentYear: string = '';
+
+  constructor() {
+    this.currentYear = new Date().getFullYear().toString();
+  }
 
   cleanVerses(text: string) {
-    let cleanedVerses = text.trim();
+    let cleanedVerses = this.processText(text);
 
-    // Mark headings
-    cleanedVerses = cleanedVerses.replace(this.HEADING_REGEX, 'HEADING_$1_HEADING\n$2');
+    if (!this.preserveWhitespace) {
+      cleanedVerses = cleanedVerses.replace(/\n/g, ' ').replace(/  +/g, ' ');
+    }
 
-    cleanedVerses = this.processText(cleanedVerses);
+    if (this.insertNewlines) {
+      cleanedVerses = cleanedVerses.replace(/ *(VERSE_[0-9]+_VERSE)/g, '\n$1');
+    }
 
     if (this.removeFootnotes) {
       cleanedVerses = cleanedVerses.replace(this.FOOTNOTE_REGEX, '');
     }
 
     if (this.removeHeadings) {
-      cleanedVerses = cleanedVerses.replace(/HEADING_.+_HEADING\n/g, '');
+      cleanedVerses = cleanedVerses.replace(/HEADING_.+_HEADING/g, '');
     } else {
-      cleanedVerses = cleanedVerses.replace(/HEADING_(.+)_HEADING\n/g, '\n$1\n\n');
+      cleanedVerses = cleanedVerses.replace(/HEADING_(.+)_HEADING/g, '\n$1\n');
     }
 
-    // TODO: Remove excessive newlines / add option for removing newlines
+    if (this.removeVerseNumbers) {
+      cleanedVerses = cleanedVerses.replace(/VERSE_([0-9]+)_VERSE /g, '');
+    } else {
+      cleanedVerses = cleanedVerses.replace(/VERSE_([0-9]+)_VERSE/g, '$1');
+    }
 
-    // Clean up
-    cleanedVerses = cleanedVerses.replace(/\n\n+/g, '\n').replace(/\n /g, '\n').trim();
-
-    this.results = cleanedVerses;
+    this.results = cleanedVerses.trim();
   }
 
   processText(textToProcess: string): string {
     let processedText;
+
+    // Add a newline at the beginning - needed for the heading regex to process correctly
+    textToProcess = `\n${textToProcess}`;
+    textToProcess = textToProcess.replace(this.HEADING_REGEX, 'HEADING_$1_HEADING$2');
 
     const firstNumberMatch = this.FIRST_NUMBER_REGEX.exec(textToProcess);
     if (firstNumberMatch) {
@@ -57,16 +71,10 @@ export class AppComponent {
       let nextVerseRegex = new RegExp(`^${verseNum} `);
       while (unprocessedText.length > 0) {
         if (nextVerseRegex.test(unprocessedText.join(''))) {
-          if (this.insertNewlines) {
-            processedText += '\n';
-          }
-
-          if (!this.removeVerseNumbers) {
-            processedText += `${verseNum} `;
-          }
+          processedText += `VERSE_${verseNum}_VERSE`;
           unprocessedText.splice(0, verseNum.length);
 
-          // TODO: Add handling for crossing over chapters
+          // TODO: Add handling for chapter numbers
           verseNum = `${parseInt(verseNum) + 1}`;
           nextVerseRegex = new RegExp(`^${verseNum} `);
         } else {
